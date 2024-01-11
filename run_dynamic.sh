0ce74e7f039e2149f8b40caf2cf07a0cc62af10b
Aditya <akkamath@uw.edu>
2024-01-11 22:08:13 +0000

    Triple number of tokens generated by LLAMA2 to prevent premature completion

--- run_dynamic.sh
+++ run_dynamic.sh
@@ -21,7 +21,7 @@ HOTFRAC2=0.30
 nice -20 numactl -N0 -m0 --physcpubind=0-3 -- ./src/central-manager >$debugfile 2>&1 &
 central_pid=$!
 sleep 30
-nice -20 numactl -N0 -m0 --physcpubind=19-23 -- env MISS_RATIO=1.0 LD_PRELOAD=/home/amanda/hemem/src/libhemem-llama.so ./apps/llama.cpp/main -m /mnt/sda1/LLaMa2/Llama-2-70b-hf/ggml-model-f16.gguf --threads 4 -p "The key to happiness in one short sentence is:" -n 40 -e > data/dynamic/perf/llama.txt 2>&1 &
+nice -20 numactl -N0 -m0 --physcpubind=19-23 -- env MISS_RATIO=1.0 LD_PRELOAD=/home/amanda/hemem/src/libhemem-llama.so ./apps/llama.cpp/main -m /mnt/sda1/LLaMa2/Llama-2-70b-hf/ggml-model-f16.gguf --threads 4 -p "The key to happiness in one short sentence is:" -n 120 -e > data/dynamic/perf/llama.txt 2>&1 &
 llama_pid=$!
 perf stat -e instructions -I 1000 -p ${llama_pid} -o data/dynamic/perf/llama-ipc.txt &
 ./wait-llama.sh data/dynamic/perf/llama.txt
